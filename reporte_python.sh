dcd4e4ae512825b10a8d54a1b374f09bcd4e25cf
sebastianVP <alex.valdezp22@gmail.com>
2025-02-18 17:46:56 -0500

    update log

--- reporte_python.sh
+++ reporte_python.sh
@@ -1,2 +1,6 @@
-LOGFILE=$(pwd)"/LOGFILE_REPORTE_ON.txt"
+#!/bin/bash
+# Obtener la ruta del script (directorio donde está ubicado)
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+LOGFILE="$SCRIPT_DIR/LOGFILE_REPORTE_ON.txt"
 /home/soporte/anaconda3/envs/schain3/bin/python3.8 /home/soporte/Documents/AMISR_REPORTE_MENSUAL/reporte_python.py > $LOGFILE
